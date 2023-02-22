83a63ac370d07cca0a2cf087950a54ed918c2fb8
Cody Chaplin <chaplinbcody@gmail.com>
2023-02-22 12:24:44 -0400

    Fixed async db init crashing issue.

--- MoMoney/MoMoneydb.cs
+++ MoMoney/MoMoneydb.cs
@@ -6,35 +6,45 @@ namespace MoMoney
     public static class MoMoneydb
     {
         public static SQLiteAsyncConnection db { get; private set; }
+        static readonly SemaphoreSlim semaphore = new(1);
 
         /// <summary>
         /// Creates new database connection, creates tables if not exists and adds default data to tables.
         /// </summary>
         public static async Task Init()
         {
-            if (db is not null)
-                return;
-            
-            db = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            await semaphore.WaitAsync();
 
-            await db.CreateTableAsync<Transaction>();
-            await db.CreateTableAsync<Account>();
-            await db.CreateTableAsync<Stock>();
+            try
+            {
+                if (db is not null)
+                    return;
 
-            var category = await db.CreateTableAsync<Category>();
-            if (category == CreateTableResult.Created)
-            {
-                await db.InsertAllAsync(GetDefaultCategories());
-            }
-            else
-            {
-                int count = await db.Table<Category>().CountAsync();
-                if (count < 1)
+                db = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+
+                await db.CreateTableAsync<Transaction>();
+                await db.CreateTableAsync<Account>();
+                await db.CreateTableAsync<Stock>();
+
+                var category = await db.CreateTableAsync<Category>();
+                if (category == CreateTableResult.Created)
                 {
-                    await db.DropTableAsync<Category>();
-                    await db.CreateTableAsync<Category>();
                     await db.InsertAllAsync(GetDefaultCategories());
                 }
+                else
+                {
+                    int count = await db.Table<Category>().CountAsync();
+                    if (count < 1)
+                    {
+                        await db.DropTableAsync<Category>();
+                        await db.CreateTableAsync<Category>();
+                        await db.InsertAllAsync(GetDefaultCategories());
+                    }
+                }
+            }
+            finally
+            {
+                semaphore.Release();
             }
         }
 
