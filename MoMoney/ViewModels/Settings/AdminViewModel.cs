cff7d949d552a0f510e5449787781b6a8db0f430
Cody Chaplin <codybchaplin@gmail.com>
2023-06-12 19:27:22 -0400

    Updated Settings and Stats pages UI.

--- MoMoney/ViewModels/Settings/AdminViewModel.cs
+++ MoMoney/ViewModels/Settings/AdminViewModel.cs
@@ -115,12 +115,12 @@ public partial class AdminViewModel : ObservableObject
         //await Shell.Current.GoToAsync(nameof(LoggingPage));
     }
 
-    /// <summary>
+    /*/// <summary>
     /// Goes to BulkEditingPage.xaml.
     /// </summary>
     [RelayCommand]
     async Task GoToBulkEditing()
     {
-        //await Shell.Current.GoToAsync(nameof(BulkEditingPage));
-    }
+        await Shell.Current.GoToAsync(nameof(BulkEditingPage));
+    }*/
 }
